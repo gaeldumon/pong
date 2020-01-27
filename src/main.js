ea1ff234b1bbdacac9f283938461ef334b3fcfa0
= <slotuser@gmail.com>
2020-01-27 10:42:42 +0100

    Moved score to Pad(), deleted Scoreboard object, refactored ball-pad collisions.

--- src/main.js
+++ src/main.js
@@ -1,21 +1,4 @@
-function ScoreBoard() {
-	this.load = function(px) {
-		this.x = px;
-		this.y = 50;
-		this.color = '#fb2eeb';
-		this.size = 50;
-		this.score = 0;
-	}
-
-	this.draw = function() {
-		push();
-		textAlign(CENTER);
-		textSize(this.size);
-		fill(this.color);
-		text(this.score, this.x, this.y);
-		pop();
-	}
-}
+/**26/01/2020 : To fix => no collision with left pad !**/
 
 const net = {
 	load: function() {
@@ -35,22 +18,46 @@ const net = {
 	}
 }
 
-function Pad() {
+/******************************************************************************/
 
-	this.load = function(pSide) {
+function Pad(pSide) {
 
+	this.load = function() {
 		this.type = pSide;
-
-		if (this.type === 'left')
-			this.x = 100;
-		else if (this.type === 'right')
-			this.x = width - 100;
-
 		this.y = height / 2;
 		this.color = '#06fdff';
 		this.w = 10;
 		this.h = 50;
 		this.vy = 4;
+		this.score = {
+			value: 0,
+			x: 100,
+			y: 50,
+			color: '#fb2eeb',
+			size: 50
+		};
+
+		this.limits = {
+			top: 0 + this.h/2,
+			bottom: height - this.h/2
+		};
+
+		if (this.type === 'left') {
+			this.x = 100;
+			this.setControls(asciiCodeKeyboard['z'], asciiCodeKeyboard['s']);
+
+		} else if (this.type === 'right') {
+			this.x = width - 100;
+			this.score.x = this.x;
+			this.setControls(UP_ARROW, DOWN_ARROW);
+		}
+	}
+
+	this.drawScore = function() {
+		textAlign(CENTER);
+		textSize(this.score.size);
+		fill(this.score.color);
+		text(this.score.value, this.score.x, this.score.y);
 	}
 
 	this.setControls = function(key1, key2) {
@@ -68,29 +75,30 @@ function Pad() {
 		this.y += this.vy * (deltaTime/10);
 	}
 
-	this.bottomBorderStop = function() {
-		if (this.y >= height - this.h/2) {
-			this.y = height - this.h/2;
-		} 
-	}
+	this.borderStop = function() {
+		if (this.y <= this.limits.top) {
 
-	this.topBorderStop = function() {
-		if (this.y <= this.h/2) {
-			this.y = this.h/2;
-		} 
+			this.y = this.limits.top;
+
+		} else if (this.y >= this.limits.bottom) {
+
+			this.y = this.limits.bottom;
+		}
 	}
 
 	this.update = function() {
-		if (keyIsDown(this.controls.up))
+		if (keyIsDown(this.controls.up)) {
 			this.moveUp();
-		else if (keyIsDown(this.controls.down))
+		} else if (keyIsDown(this.controls.down)) {
 			this.moveDown();
+		}
 
-		this.bottomBorderStop();
-		this.topBorderStop();
+		this.borderStop();
 	}
 
 	this.draw = function() {
+		this.drawScore();
+
 		push();
 		rectMode(CENTER);
 		fill(this.color);
@@ -99,8 +107,9 @@ function Pad() {
 	}
 }
 
-function Ball() {
+/******************************************************************************/
 
+function Ball() {
 	this.load = function() {
 		this.x = width / 2;
 		this.y = height / 2;
@@ -108,8 +117,12 @@ function Ball() {
 		this.color = '#fefe4e';
 		this.vx = 6;
 		this.vy = 6;
-		this.speed = 6;
 		this.direction = 1;
+
+		this.limits = {
+			top: 0 + this.radius,
+			bottom: height - this.radius
+		};
 	}
 
 	this.reset = function() {
@@ -117,49 +130,43 @@ function Ball() {
 		this.y = height/2;
 	}
 
-	this.faster = function() {
-		//this.speed++;
-	}
+	this.borderBounce = function() {
+		if (this.y <= this.limits.top) {
 
-	this.bottomBorderBounce = function() {
-		if (this.y >= height - this.radius) {
-			this.y = height - this.radius;
+			this.y = this.limits.top;
 			this.vy = 0 - this.vy;
-		} 
-	}
 
-	this.topBorderBounce = function() {
-		if (this.y <= this.radius) {
-			this.y = this.radius;
+		} else if (this.y >= this.limits.bottom) {
+
+			this.y = this.limits.bottom;
 			this.vy = 0 - this.vy;
-		} 
-	}
-
-	this.rightPadCollide = function(pad) { 
-		if (this.x >= (pad.x - pad.w/2 - this.radius) &&
-			/*Checking if this is on the FRONT side of the pad and not behind*/
-			this.x < (pad.x + pad.w/2 - this.radius) &&
-			this.y >= (pad.y - pad.h/2) &&
-			this.y <= (pad.y + pad.h/2)) {
-
-			this.faster();
-			/*Repositioning the this a bit (outside pad) to avoid some bug*/
-			this.x -= 1;
-			this.vx = 0 - this.vx;
 		}
 	}
 
-	this.leftPadCollide = function(pad) {
-		if (this.x <= (pad.x + pad.w/2 + this.radius) &&
-			/*Checking if this is on the FRONT side of the pad and not behind*/
-			this.x > (pad.x - pad.w/2 + this.radius) &&
-			this.y >= (pad.y - pad.h/2) &&
-			this.y <= (pad.y + pad.h/2)) {
+	this.padCollide = function(pads) {
+		for (const pad of pads) {
 
-			this.faster();
-			/*Repositioning the this a bit (outside pad) to avoid some bug*/
-			this.x += 1;
-			this.vx = 0 - this.vx;
+			if (this.y >= (pad.y - pad.h/2) && this.y <= (pad.y + pad.h/2)) {
+
+				if (pad.type === 'right') {
+
+					if (this.x >= (pad.x - pad.w/2 - this.radius) &&
+						this.x < (pad.x + pad.w/2 - this.radius)) {
+
+						this.x -= 1;
+						this.vx = 0 - this.vx;
+					}
+
+				} else if (pad.type === 'left') {
+
+					if (this.x <= (pad.x - pad.w/2 - this.radius) &&
+						this.x > (pad.x - pad.w/2 + this.radius)) {
+
+						this.x += 1;
+						this.vx = 0 - this.vx;
+					}
+				}
+			}
 		}
 	}
 
@@ -171,80 +178,67 @@ function Ball() {
 		this.direction = -1;
 	}
 
-	this.update = function(scoreBoardLeft, scoreBoardRight, padLeft, padRight) {
+	this.update = function(pads) {
 		this.x += this.direction * (this.vx * (deltaTime/30));
 		this.y += this.direction * (this.vy * (deltaTime/30));
 
-		this.rightPadCollide(padRight);
-		this.leftPadCollide(padLeft);
+		//Loop through pads given as parameter to detect collisions
+		this.padCollide(pads);
 
-		this.bottomBorderBounce();
-		this.topBorderBounce();
+		this.borderBounce();
 
-		if (this.x > width) {
-			scoreBoardLeft.score++;
-			this.serveRight();
-			this.reset();
+		for (const pad of pads) {
+			if (pad.type === 'left' && this.x >= width) {
+				pad.score.value++;
+				this.serveRight();
+				this.reset();
 
-		} else if (this.x < 0) {
-			scoreBoardRight.score++;
-			this.serveLeft();
-			this.reset();
+			} else if (pad.type === 'right' && this.x <= 0) {
+				pad.score.value++;
+				this.serveLeft();
+				this.reset();
+			}
 		}
 	}
 
 	this.draw = function() {
 		push();
 		fill(this.color);
-		circle(this.x, this.y, this.radius*2);
+		circle(this.x, this.y, this.radius * 2);
 		pop();
 	}
 }
 
-let scoreBoard1 = new ScoreBoard();
-let scoreBoard2 = new ScoreBoard();
+const pad1 = new Pad('left');
+const pad2 = new Pad('right');
+const pads = [pad1, pad2];
 
 const ball = new Ball();
-const pad1 = new Pad();
-const pad2 = new Pad();
 
 function setup() {
 	createCanvas(800, 400);
 	background('#1b002a');
 	noStroke();
 
-	/*Loading left score board at 40px on the X axis*/
-	scoreBoard1.load(40);
-	/*Loading right score board at 760px on the X axis*/
-	scoreBoard2.load(width - 40);
+	for (const pad of pads) {
+		pad.load();
+	}
 
 	ball.load();
 
-	/*Loading left pad at 100px (cf. pad object) on the X axis*/
-	pad1.load('left');
-	/*Left pad is controlled with the z and s keyboard keys*/
-	pad1.setControls(asciiCodeKeyboard['z'], asciiCodeKeyboard['s']);
-
-	/*Loading right pad at 700px (cf. pad object) on the X axis*/
-	pad2.load('right');
-	/*Right pad is controlled with the up and down arrow*/
-	pad2.setControls(UP_ARROW, DOWN_ARROW);
-
-	/*Loading the net, offering a graphic separation of the game screen*/
 	net.load();
 }
 
 function draw() {
 	background('#1b002a');
 
-	ball.update(scoreBoard1, scoreBoard2, pad1, pad2);
-	pad1.update();
-	pad2.update();
+	for (const pad of pads) {
+		pad.update();
+		pad.draw();
+	}
 
-	scoreBoard1.draw();
-	scoreBoard2.draw();
+	ball.update(pads);
+	ball.draw();
+
 	net.draw();
-	ball.draw()
-	pad1.draw();
-	pad2.draw();
 }
